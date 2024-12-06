3ed70f8f758a55f29c6210a13c57d277338491cf
fasminoufal <fasminoufal@gmail.com>
2024-12-06 12:41:18 +0530

    chane color

---
+++ button1.js
@@ -0,0 +1,10 @@
+const output = document.getElementById("button")
+const colours=["red","blue","green","yellow"];
+let currentIndex =0;
+const bg=document.getElementById("changebg")
+function changeColor() {
+    output.style.backgroundColor = colours[currentIndex]
+    currentIndex= (currentIndex +1)%colours.length
+   bg.style.background= "blue"
+}
+
